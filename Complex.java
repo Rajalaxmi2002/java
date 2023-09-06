2d087804ad548dcb94152217c1d40b7eeee3d7a1
Rajalaxmi2002 <119930935+Rajalaxmi2002@users.noreply.github.com>
2023-09-06 21:47:05 +0530

    add two complex number

--- Complex.java
+++ Complex.java
@@ -1,29 +1,53 @@
+import java.util.Scanner;
 class Complex
 {
-int r;
-int i;
-Complex(int real,int img)
+int rp,ip;
+Complex(int r,int i)
 {
-r=real;
-i=img;
+rp=r;
+ip=i;
 }
-void display()
+void sum(Complex c1,Complex c2)
 {
-System.out.println(r+"+"+i+"i");
+int rsum=c1.rp+c2.rp;
+int isum=c1.ip+c2.ip;
+System.out.println(rsum+"+"+isum)
 }
-static void add(int r1,int i1,int r2,int i2)
+Public class Complex_program
 {
-r1=r1+r2;
-i1=i1+i2;
-System.out.println("after addition="+r1+"+"+i1+"i");
-}
-public static void main(String[]args)
+Public static void main(String []args)
 {
-Complex first=new Complex(5,4);
-Complex second=new Complex(7,9);
-System.out.println("complex numbers are:");
-first.display();
-second.display();
-add(first.r,first.i,second.r,second.i);
+Scanner sc=new Scanner(System.in)
+System.out.println("enter the first number");
+System.out.println("enter real part");
+int frp=sc.nextInt();
+System.out.println("enter imaginary part");
+int fip=sc.nextInt();
+Sysytem.out.println(frp+"+"+fip+"i");
+System.out.println("enter the second number");
+System.out.println("enter real part");
+int srp=sc.nextInt();
+System.out.println("enter imaginary part");
+int sip=sc.nextInt();
+Sysytem.out.println(srp+"+"+sip+"i");
+Complex c1=new Complex(frp,fip)
+Complex c2=new Complex(srp,sip)
+System.out.println("after addition");
+c1.sum(c1,c2);
 }
 }
+
+enter the first number
+enter real part
+4
+enter imaginary part
+6
+4+6i
+enter the second number
+enter real part
+7
+enter imaginary part
+9
+7+9i
+after addition
+11+15i
